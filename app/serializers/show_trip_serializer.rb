8a01ad7d9f5ed0e9ffa3e634fe37eef0d54c48b0
Itay Adler <itayadler@gmail.com>
2013-10-29 22:48:15 +0200

    more awesome stop information for trip

--- app/serializers/show_trip_serializer.rb
+++ app/serializers/show_trip_serializer.rb
@@ -1,11 +1,12 @@
 class ShowTripSerializer < ActiveModel::Serializer
-  attributes :id, :direction_id
+  attributes :id, :direction_id, :stops
   has_one :route
   has_one :path
-  has_many :stops
 
   def stops
-    object.stop_times.sort { |a,b| a.stop_sequence <=> b.stop_sequence }.map(&:stop)
+    object.stop_times.sort { |a,b| a.stop_sequence <=> b.stop_sequence }.map do |stop_time|
+      stop_time.as_json.merge(stop_time.stop.as_json).except("trip_id", "departure_time")
+    end
   end
 end
 
