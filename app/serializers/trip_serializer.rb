26da013e83c369aa09c4c97e2fd77d835d2537b3
Itay Adler <itayadler@gmail.com>
2013-10-31 14:37:31 +0200

    added origin and destination to trip

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,20 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :shape_id
+  attributes :id, :shape_id, :origin, :destination
   has_one :route
+
+  attr_accessor :_stops
+
+  def origin
+    self.stops.first.name
+  end
+
+  def destination
+    self.stops.last.name
+  end
+
+  def stops
+    self._stops ||= object.stop_times.sort { |a,b| a.stop_sequence <=> b.stop_sequence }.map(&:stop)
+  end
+
 end
 
